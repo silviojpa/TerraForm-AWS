91ef85002c329e88153fea69ab7515c4b24b3f87
root <silviop.ezequiel@gmail.com>
2023-11-03 00:33:53 -0300

    alteração de ec2

--- main.tf
+++ main.tf
@@ -5,9 +5,40 @@ provider "aws" {
 resource "aws_instance" "example_server" {
   ami           = "ami-0af6e9042ea5a4e3e"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+     sudo apt update -y
+     sudo apt install apache2 -y
+     sudo systemctl start apache2
+     sudo bash -c 'echo Hello World > /var/www/html/index.html'
+     EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "UbuntuTeste-silvio"
   }
   
+}
+
+
+resource "aws_security_group" "instance" {
+	name = "teste_servidor-silvio"
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+  }
+
 }
